687903a9533f2db33566b314744c4aa2363fd0e6
YuliaSukhomlinova <yulia_alekseeva1997@mail.ru>
2017-07-14 22:22:57 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i, n, p = 0;
+            string str = "8-1+100-99+345-777+45";
+            string[] s = str.Split('+', '-');
+            n = s.Length;
+
+            for (i = 0; i < n; i++)
+            {
+                if (Convert.ToInt32(i) % 2 == 0)
+                    p += Convert.ToInt32(s[i]);
+                else
+                    p -= Convert.ToInt32(s[i]);
+            }
+            Console.WriteLine(str + " = " + p);
+            Console.ReadKey();
+        }
+    }
+}
